92e608d41870fa3b40850dfbd09aedc6887e1794
ravipandya15 <38007249+ravipandya15@users.noreply.github.com>
2024-09-29 22:55:02 +0530

    Update GFG_First_negative_integer_in_every_window_of_size_k.cpp

--- Sliding_Window_Aditya_Verma/Fixed_Sized_Sliding_Window/GFG_First_negative_integer_in_every_window_of_size_k.cpp
+++ Sliding_Window_Aditya_Verma/Fixed_Sized_Sliding_Window/GFG_First_negative_integer_in_every_window_of_size_k.cpp
@@ -10,7 +10,7 @@ int main()
 
 vector<long long> printFirstNegativeInteger(long long int A[], long long int N, long long int K) {
     vector<long long> ans;
-    deque<long long> dequeue;
+    deque<long long> deque;
     int i = 0;
     int j = 0;
 
@@ -19,7 +19,7 @@ vector<long long> printFirstNegativeInteger(long long int A[], long long int N, 
         // calculation
         if (A[j] < 0)
         {
-            dequeue.push_back(A[j]);
+            deque.push_back(A[j]);
         }
 
         if (j-i+1 < K)
@@ -29,20 +29,20 @@ vector<long long> printFirstNegativeInteger(long long int A[], long long int N, 
         else if (j - i + 1 == K)
         {
             // ans from calculation
-            if (dequeue.empty())
+            if (deque.empty())
             {
                 ans.push_back(0);
             }
             else
             {
-                ans.push_back(dequeue.front());
+                ans.push_back(deque.front());
             }
 
 
             // slide the window
-            if (A[i] == dequeue.front())
+            if (A[i] == deque.front())
             {
-                dequeue.pop_front();
+                deque.pop_front();
             }
 
             j++;
